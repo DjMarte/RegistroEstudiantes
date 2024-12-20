7e391ff7e86d6a2776435d214e72db5714f53c96
DjMarte <cristopher_marte@ucne.edu.do>
2024-12-19 21:11:46 -0400

    Se creó la carpeta Services y el servicio del modelo Estudiante.

---
+++ RegistroEstudiante/Services/EstudiantesService.cs
@@ -0,0 +1,63 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using RegistroEstudiante.DAL;
+using RegistroEstudiante.Models;
+
+namespace RegistroEstudiante.Services;
+
+/*
+    1. Guardar
+    2. Existe (privado)
+    3. Insertar (privado)
+    4. Modificar (privado)
+    5. Buscar
+    6. Eliminar
+    7. Listar
+*/
+
+public class EstudiantesService(Contexto _contexto){
+    public async Task<bool> Guardar(Estudiantes estudiante){
+        if(! await Existe(estudiante.EstudianteId))
+            return await Insertar(estudiante);
+        else
+            return await Modificar(estudiante);
+    }
+
+    private async Task<bool> Existe(int estudianteId)
+    {
+        return await _contexto.Estudiantes
+            .AnyAsync(e => e.EstudianteId == estudianteId);
+    }
+
+    private async Task<bool> Insertar(Estudiantes estudiante)
+    {
+        _contexto.Estudiantes.Add(estudiante);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    private async Task<bool> Modificar(Estudiantes estudiante)
+    {
+        _contexto.Estudiantes.Update(estudiante);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<Estudiantes?> Buscar(int estudianteId){
+        return await _contexto.Estudiantes
+            .AsNoTracking()
+            .FirstOrDefaultAsync(e => e.EstudianteId == estudianteId);
+    }
+
+    public async Task<bool> Eliminar(int estudianteId){
+        return await _contexto.Estudiantes
+            .AsNoTracking()
+            .Where(e => e.EstudianteId == estudianteId)
+            .ExecuteDeleteAsync() > 0;
+    }
+
+    public async Task<List<Estudiantes>> Listar(Expression<Func<Estudiantes, bool>> criterio){
+        return await _contexto.Estudiantes
+            .AsNoTracking()
+            .Where(criterio)
+            .ToListAsync();
+    }
+}
