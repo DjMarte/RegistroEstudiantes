cf2530b8ce34967d22e1c4f6b4366345687a9b7b
DjMarte <cristopher_marte@ucne.edu.do>
2024-12-19 20:18:53 -0400

    Se creó la carpeta DAL y el contexto.

---
+++ RegistroEstudiante/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using RegistroEstudiante.Models;
+
+namespace RegistroEstudiante.DAL;
+
+public class Contexto : DbContext{
+
+    //     Nombre    //Tipo de dato            // identidicador
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Estudiantes> Estudiantes { get; set; }
+}
