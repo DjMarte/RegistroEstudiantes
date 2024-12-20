91aab784c80f187e9e8c86d52e824112fd921b85
DjMarte <cristopher_marte@ucne.edu.do>
2024-12-19 20:27:14 -0400

    Inyecció del Contexto en el Program.

--- RegistroEstudiante/Program.cs
+++ RegistroEstudiante/Program.cs
@@ -1,10 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using RegistroEstudiante.Components;
+using RegistroEstudiante.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+
+// Inyeccion del Contexto
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Contexto>(o => o.UseSqlite(ConStr));
 
 var app = builder.Build();
 
