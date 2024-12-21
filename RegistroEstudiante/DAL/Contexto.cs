d8fcdcdc3d0acd43a361b531eb8e428c2a93f0a7
DjMarte <cristopher_marte@ucne.edu.do>
2024-12-20 21:07:56 -0400

    Agregando comentario.

--- RegistroEstudiante/DAL/Contexto.cs
+++ RegistroEstudiante/DAL/Contexto.cs
@@ -8,5 +8,5 @@ public class Contexto : DbContext{
     //     Nombre    //Tipo de dato            // identidicador
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
-    public DbSet<Estudiantes> Estudiantes { get; set; }
+    public DbSet<Estudiantes> Estudiantes { get; set; } // Tabla Estudiante
 }
